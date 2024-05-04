bc441d113859508b9c14970b52c04c88387fedf9
abhijithchowdary2 <abhijithchowdary2@gmail.com>
2024-05-04 18:04:31 +0530

    adding s to http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "http://postappbackend-g4ff.onrender.com/api"
+    apiUrl: "https://postappbackend-g4ff.onrender.com/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: "http://localhost:3000/api"
-    apiUrl: "http://postappbackend-g4ff.onrender.com/api"
+    apiUrl: "https://postappbackend-g4ff.onrender.com/api"
 }
